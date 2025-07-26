022a6c3b86d7507062b2b751182de5a1151bc494
NKDuyennn <kieuduyennn2310@gmail.com>
2025-07-26 12:15:26 +0700

    learn bower laravel mix

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,9 +9,18 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+                    {{ __('You are logged in!') }}
                 </div>
             </div>
+        
+            <div class="mt-4">
+                <x-input-label for="task" :value="__('Task')" />
+                <x-text-input id="task" name="task" type="text" class="mt-1 block w-full" required autocomplete="task" />
+                <x-input-error :messages="$errors->get('task')" class="mt-2" />
+            </div>
+            <x-primary-button class="mt-4">
+                {{ __('Add Task') }}
+            </x-primary-button>
         </div>
     </div>
 </x-app-layout>
